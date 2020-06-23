5f3cdade32952e7af595eafcd1b8a6224d388e05
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-23 15:03:47 -0400

    q5

--- workshop/exercise-5.js
+++ workshop/exercise-5.js
@@ -2,19 +2,23 @@
 // ----------
 
 const foodPairings = {
-  burgers: 'beer',
-  fish: 'white wine',
-  beef: 'red wine',
-  strawberries: 'cream',
+  burgers: "beer",
+  fish: "white wine",
+  beef: "red wine",
+  strawberries: "cream",
 };
 
-const getValue = () => {
-  // do something
+const getValue = (Obj, key) => {
+  if (Object.keys(Obj).includes(key)) {
+    return Obj[key];
+  } else {
+    return "not available";
+  }
 };
 
-expect(getValue(foodPairings, 'burgers'), 'beer');
-expect(getValue(foodPairings, 'beef'), 'red wine');
-expect(getValue(foodPairings, 'sushi'), 'not available');
+expect(getValue(foodPairings, "burgers"), "beer");
+expect(getValue(foodPairings, "beef"), "red wine");
+expect(getValue(foodPairings, "sushi"), "not available");
 
 /**
  * -------------------------------------------------------------------
@@ -23,7 +27,7 @@ expect(getValue(foodPairings, 'sushi'), 'not available');
  */
 function expect(result, value) {
   if (result === value) {
-    console.log('✅ Test succeeded');
+    console.log("✅ Test succeeded");
   } else {
     console.log(`⛔️ Expected “${result}” to equal “${value}”`);
   }
