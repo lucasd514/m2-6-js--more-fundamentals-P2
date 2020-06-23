18f75b4780ec217cc5994543abde99ca0633876b
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-23 13:32:17 -0400

    e2

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -1,5 +1,13 @@
 // Exercise 2
 // ----------
 
-const lateComers = ['Felicia', 'Charles', 'Terry'];
-const earlyBirds = ['Ellen', 'Jerry', 'Jasmine'];
+const lateComers = ["Felicia", "Charles", "Terry"];
+const earlyBirds = ["Ellen", "Jerry", "Jasmine"];
+
+printGuestOrder = (early, late) => {
+  let newarray = [...early, ...late];
+
+  guests.forEach((attendee, index) => {
+    console.log(`${index + 1}. ${attendee}`);
+  });
+};
