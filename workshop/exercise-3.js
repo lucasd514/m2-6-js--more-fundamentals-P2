eed8c82eab1f4143b110037a2a792cabe053cf8f
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-23 14:13:35 -0400

    q3

--- workshop/exercise-3.js
+++ workshop/exercise-3.js
@@ -2,14 +2,27 @@
 // ----------
 
 let mostPopularFood = [
-  'bean burritos',
-  'poke bowl',
-  'chicken sliders',
-  'pork baby back ribs',
-  'chicken burritos',
-  'chicken sandwich',
-  'cauliflower rice bowl',
-  'chicken and waffle sliders',
-  'parmesan chicken',
-  'buffalo-flavored cauliflower',
+  "bean burritos",
+  "poke bowl",
+  "chicken sliders",
+  "pork baby back ribs",
+  "chicken burritos",
+  "chicken sandwich",
+  "cauliflower rice bowl",
+  "chicken and waffle sliders",
+  "parmesan chicken",
+  "buffalo-flavored cauliflower",
 ];
+
+function sortAlphabetically(list) {
+  return [...list]
+    .sort((a, b) => {
+      if (a > b) return 1;
+      if (a < b) return -1;
+    })
+    .forEach((item) => {
+      let rank = mostPopularFood.indexOf(item);
+      console.log(`${item} (${rank + 1})`);
+    });
+}
+sortAlphabetically(mostPopularFood);
