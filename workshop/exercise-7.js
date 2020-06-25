dfabe80db6487e811e83467a768e7424da39021f
lucas dalessandro <lucas.dalessandro@gmail.com>
2020-06-25 08:24:33 -0400

    min completetd

--- workshop/exercise-7.js
+++ workshop/exercise-7.js
@@ -2,15 +2,23 @@
 // ----------
 
 let foodPairings = {
-  burgers: 'beer',
-  fish: 'white wine',
-  beef: 'red wine',
-  strawberries: 'cream',
+  burgers: "beer",
+  fish: "white wine",
+  beef: "red wine",
+  strawberries: "cream",
+};
+//new array
+const foodDuos = function (arr) {
+  return Object.keys(arr).map((cibo) => {
+    return "with " + cibo + " it is best to have " + arr[cibo];
+  });
 };
 
-// createRecommendations function
-
-// printRecommendations function
-
-// function call (done)
-printRecommendations(createRecommendations(foodPairings));
+///print function using new array
+const printergoBrr = (arr) => {
+  arr.forEach((ele) => {
+    return console.log("-" + ele);
+  });
+};
+//create new array, print it all at once
+printergoBrr(foodDuos(foodPairings));
